daf29f9bbd4a3f86783d618d3738cad4f78f9fd7
Ahmed <dr.ahmed.e.amer@gmail.com>
2017-01-16 17:48:49 +0200

    sound object

--- source/soundManager.js
+++ source/soundManager.js
@@ -1,6 +1,14 @@
  function soundManager() {
-     var mute = false;
+     var audios = {
+         brick : new Audio('sound/brick.mp3'),
+         paddle: new Audio('sound/paddle.mp3'),
+         start : new Audio('sound/go.mp3'),
+         lose : new Audio('sound/loselife.mp3'),
+         gameOver: new Audio('sound/gameover.mp3'),
+         levelUp : new Audio('sound/levelup.mp3')
+     }
 
+     var mute = false;
      if (arguments.callee._singletonInstance) {
          return arguments.callee._singletonInstance;
      }
@@ -9,42 +17,36 @@
 
      this.brickHit = function () {
          if (!mute) {
-             var audio = new Audio('sound/brick.mp3');
-             audio.play();
+             audios.brick.play();
          }
      };
 
      this.paddleHit = function () {
          if (!mute) {
-             var audio = new Audio('sound/paddle.mp3');
-             audio.play();
+            audios.paddle.play();
          }
      };
 
      this.gameStart = function () {
          if (!mute) {
-             var audio = new Audio('sound/go.mp3');
-             audio.play();
+           audios.start.play();
          }
      };
 
      this.loseLife = function () {
          if (!mute) {
-             var audio = new Audio('sound/loselife.mp3');
-             audio.play();
+             audios.lose.play();
          }
      };
 
      this.gameOver = function () {
          if (!mute) {
-             var audio = new Audio('sound/gameover.mp3');
-             audio.play();
+            audios.gameOver.play();
          }
      };
      this.levelUp = function () {
          if (!mute) {
-             var audio = new Audio('sound/levelup.mp3');
-             audio.play();
+            audios.levelUp.play();
          }
      };
      music = new Audio('sound/Hit_It_Hard.mp3');
