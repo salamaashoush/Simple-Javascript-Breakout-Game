29c5e179ebd3f4afbc4a8347d1014217451eb9ea
EhabGamal <ehab.g.sh@gmail.com>
2017-01-16 16:24:59 +0200

    added new splashcreens system

--- source/gui.js
+++ source/gui.js
@@ -65,6 +65,58 @@ var GUI = function(){
         playerPaddle.appendChild(paddleDiv);
         playerBall.appendChild(ballDiv);
     }
+    this.togglePlaying = function(state){
+        var splash = document.getElementById("splashCard");
+        var control = document.getElementById("splashControl");
+        var attr = splash.getAttribute("state");
+        
+        if(!state){
+            if(attr=="pending")
+                state="start";
+            if(attr=="start")
+                state="pause";
+            else if(attr=="pause")
+                state="resume";
+            else if(attr=="resume")
+                state="pause";
+            else if(attr=="finish"){
+                state="pending";
+                levelChanger(player.currentLevel);
+            }
+        }
+        
+        if(state=="start" || state=="resume"){
+            play = true;
+        }else if(state=="pending"){
+            play = false;
+            control.children[0].children[0].className = "icon-gamepad";
+            control.children[1].innerHTML = "Start Playing!";
+        }else if(state=="pause"){
+            play = false;
+            control.children[0].children[0].className = "icon-play-circled";
+            control.children[1].innerHTML = "Resume Playing!";
+        }else if(state=="gameover"){
+            play = false;
+            control.children[0].children[0].className = "icon-emo-cry";
+            control.children[1].innerHTML = "Game Over!";
+        }else if(state=="finish"){
+            play = false;
+            control.children[0].children[0].className = "icon-emo-thumbsup";
+            control.children[1].innerHTML = "Good Job!";
+        }
+        
+        if(play){
+            startGame(game);
+            splash.style.height = "0px";
+        }else{
+            splash.style.height = "501px";
+            console.log("inside else should show the splash");
+        }
+        
+        splash.setAttribute("state",state);
+        
+        console.log("game state ",state," playing ",play);
+    }
 }
 
 gui = new GUI();
